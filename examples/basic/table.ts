dd6445f5e1c97e91b1b771c81422053dd016b1d5
Bruno Rocha <brunorocha2674@gmail.com>
2023-11-17 18:19:00 -0300

    chore: add table prompt example

---
+++ examples/basic/table.ts
@@ -0,0 +1,11 @@
+import * as p from '@clack/prompts';
+import color from 'picocolors';
+
+(async () => {
+	p.table([
+		['Package', 'Current', 'Latest'].map(color.blue),
+		[`axios`, '1.6.1', '1.6.' + color.green('2')],
+		[`Prettier ${color.dim('(dev)')}`, '3.0.2', '3.' + color.yellow('1.0')],
+		[`@types/node ${color.dim('(dev)')}`, '18.16.0', color.red('20.9.1')],
+	]);
+})();
